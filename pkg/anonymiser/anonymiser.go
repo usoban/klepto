6260d912143c5d15fc68e17c4baacd913cf0bf2a
Urban Soban <u.soban@gmail.com>
2019-06-26 15:39:26 +0200

    Adds support for conditional anonymisation of columns. (#1)
    * Adds support for conditional anonymisation of columns.
    
    * Improve README.md

--- pkg/anonymiser/anonymiser.go
+++ pkg/anonymiser/anonymiser.go
@@ -7,31 +7,36 @@ import (
 	"reflect"
 	"strings"
 
+	expr "github.com/antonmedv/expr"
+	vm "github.com/antonmedv/expr/vm"
 	"github.com/hellofresh/klepto/pkg/config"
 	"github.com/hellofresh/klepto/pkg/database"
 	"github.com/hellofresh/klepto/pkg/reader"
+	option "github.com/hellofresh/klepto/pkg/util"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
 const (
 	// literalPrefix defines the constant we use to prefix literals
-	literalPrefix = "literal:"
-	email         = "EmailAddress"
-	username      = "UserName"
-	password      = "Password"
+	literalPrefix     = "literal:"
+	conditionalPrefix = "cond:"
+	email             = "EmailAddress"
+	username          = "UserName"
+	password          = "Password"
 )
 
 type (
 	anonymiser struct {
 		reader.Reader
-		tables config.Tables
+		tables        config.Tables
+		compiledRules map[string]*vm.Program
 	}
 )
 
 // NewAnonymiser returns a new anonymiser reader.
 func NewAnonymiser(source reader.Reader, tables config.Tables) reader.Reader {
-	return &anonymiser{source, tables}
+	return &anonymiser{source, tables, map[string]*vm.Program{}}
 }
 
 // ReadTable decorates reader.ReadTable method for anonymising rows published from the reader.Reader
@@ -47,6 +52,21 @@ func (a *anonymiser) ReadTable(tableName string, rowChan chan<- database.Row, op
 	if len(table.Anonymise) == 0 {
 		logger.Debug("Skipping anonymiser")
 		return a.Reader.ReadTable(tableName, rowChan, opts, matchers)
+	}
+
+	// Compile conditional anonymisation rules
+	for column, fakerType := range table.Anonymise {
+		if strings.HasPrefix(fakerType, conditionalPrefix) {
+			program, err := expr.Compile(strings.TrimPrefix(fakerType, conditionalPrefix))
+
+			if err != nil {
+				logger.WithError(err).Error("Conditional rule compilation failed")
+				continue
+			}
+
+			ruleKey := RuleKey(tableName, column)
+			a.compiledRules[ruleKey] = program
+		}
 	}
 
 	// Create read/write chanel
@@ -66,26 +86,49 @@ func (a *anonymiser) ReadTable(tableName string, rowChan chan<- database.Row, op
 					continue
 				}
 
-				for name, faker := range Functions {
-					if fakerType != name {
+				if strings.HasPrefix(fakerType, conditionalPrefix) {
+
+					env := map[string]interface{}{
+						"row":    row,
+						"column": row[column],
+						"Value": func(row database.Row, columnName string) string {
+							columnValue := row[columnName]
+
+							if columnValue == nil {
+								return ""
+							}
+
+							bytes := columnValue.([]uint8)
+
+							return string(bytes)
+						},
+						"Anon": func(fakerType string) *option.Option {
+							return option.Some(Anonymise(fakerType))
+						},
+						"Skip": func() *option.Option {
+							return option.None()
+						},
+						"IsNil": func(row database.Row, columnName string) bool {
+							return row[columnName] == nil
+						},
+					}
+
+					ruleKey := RuleKey(table.Name, column)
+					output, err := expr.Run(a.compiledRules[ruleKey], env)
+					if err != nil {
+						logger.WithError(err).Error("Eval rule runtime error")
 						continue
 					}
 
-					var value string
-					switch name {
-					case email, username:
-						b := make([]byte, 2)
-						rand.Read(b)
-						value = fmt.Sprintf(
-							"%s.%s",
-							faker.Call([]reflect.Value{})[0].String(),
-							hex.EncodeToString(b),
-						)
-					default:
-						value = faker.Call([]reflect.Value{})[0].String()
+					opt := output.(*option.Option)
+					if option.IsSome(opt) {
+						row[column] = option.Value(opt)
 					}
-					row[column] = value
+
+					continue
 				}
+
+				row[column] = Anonymise(fakerType)
 			}
 
 			rowChan <- row
@@ -97,4 +140,35 @@ func (a *anonymiser) ReadTable(tableName string, rowChan chan<- database.Row, op
 	}
 
 	return nil
+}
+
+// Anonymise generates a fake value
+func Anonymise(fakerType string) string {
+	var value string
+
+	for name, faker := range Functions {
+		if fakerType != name {
+			continue
+		}
+
+		switch name {
+		case email, username:
+			b := make([]byte, 2)
+			rand.Read(b)
+			value = fmt.Sprintf(
+				"%s.%s",
+				faker.Call([]reflect.Value{})[0].String(),
+				hex.EncodeToString(b),
+			)
+		default:
+			value = faker.Call([]reflect.Value{})[0].String()
+		}
+	}
+
+	return value
+}
+
+// RuleKey generates a key for storing VM program of specific table's column.
+func RuleKey(tableName string, columnName string) string {
+	return tableName + "." + columnName
 }
---
+++ pkg/util/option.go
@@ -0,0 +1,27 @@
+package option
+
+// Option type.
+type Option struct {
+	isSome bool
+	value  interface{}
+}
+
+func Some(value interface{}) *Option {
+	return &Option{true, value}
+}
+
+func None() *Option {
+	return &Option{false, nil}
+}
+
+func IsSome(option *Option) bool {
+	return option.isSome
+}
+
+func Value(option *Option) interface{} {
+	if option.isSome == false {
+		panic("Option does not have a value.")
+	}
+
+	return option.value
+}
